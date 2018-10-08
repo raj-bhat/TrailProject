a04c97fcfadb44d5cbcfeb6fba3c9f3fd154b40f
rbhat <bhatrajshekhar@gmail.com>
2018-10-08 16:53:47 +0530

    Lat-180

--- src/demoseleniunm/testfbreg.java
+++ src/demoseleniunm/testfbreg.java
@@ -1,9 +1,11 @@
 package demoseleniunm;
 
-public class testfbregp {
+public class testfbreg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("new change");
+		String a;
 
 	}
 
