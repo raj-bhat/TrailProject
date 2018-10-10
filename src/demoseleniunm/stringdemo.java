2b88ada2e3fc11604b2481366fd080923de1c149
rbhat <bhatrajshekhar@gmail.com>
2018-10-10 20:08:45 +0530

    string 2

--- src/demoseleniunm/stringdemo.java
+++ src/demoseleniunm/stringdemo.java
@@ -3,11 +3,25 @@ package demoseleniunm;
 public class stringdemo {
 
 	public static void main(String[] args) {
-		String name="Rajashekhara Bhata";
+		String name="Raashekhara Bhata";
 		boolean Status=name.startsWith("Raj");
 		boolean Status1=name.endsWith("Raj");
+		System.out.println("chat at " +name.charAt(0));
 		
 		boolean test=name.equalsIgnoreCase("rajashekhara bhata");
+		
+		String []toolname=name.split(" ");
+	
+		for (int i=0;i<toolname.length;i++){
+			System.out.println(toolname[i]);
+			
+			if(toolname[i].equalsIgnoreCase("Rajashekhara")){
+				System.out.println("match found");
+				break;
+			}
+			
+		}
+	
 		
 		System.out.println(Status);
 		System.out.println(Status1);
